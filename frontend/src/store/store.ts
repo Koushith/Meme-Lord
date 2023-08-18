5dde4133ff279bc0f07b48b5ecab477c0a66cc6e
koushith <koushith97@gmail.com>
2023-08-18 23:43:55 +0530

    wtf is this

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,14 @@
 import { apiSlice } from "@/slices/apiSlice";
-
+import authReducer from "@/slices/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
     // TODO: add providers
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware), // all these are boilderplate stuff
+  devTools: true,
 });
